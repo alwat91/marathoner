fd48353bd60854a48bf61907f3304cf52f14c066
alan <alwat91@uga.edu>
2017-03-07 17:33:13 -0500

    Refactored time helper

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -3,12 +3,7 @@ class RunsController < ApplicationController
 
 
   def create
-    new_run = current_user.runs.new({
-      mileage: run_params[:mileage],
-      beginning_time: run_params[:beginning_time],
-      end_time: run_params[:end_time],
-      duration_in_secs: duration_in_secs
-      })
+    new_run = current_user.runs.new(time_handler)
 
     if new_run.save
       render json: {status: 202, message: "Run created"}
@@ -21,10 +16,26 @@ class RunsController < ApplicationController
     render json: { status: 200, runs: current_user.runs }
   end
 
+  def update
+    run = current_user.runs.find(params[:id])
+
+    run.update(run_params)
+
+    render json: { status: 200, runs: current_user.runs}
+
+  end
+
   private
 
-  def duration_in_secs
-    run_params[:end_time].to_time - run_params[:beginning_time].to_time
+  def time_handler
+    secs = run_params[:end_time].to_time - run_params[:beginning_time].to_time
+
+    {
+      mileage: run_params[:mileage],
+      beginning_time: run_params[:beginning_time],
+      end_time: run_params[:end_time],
+      duration_in_secs: secs
+      }
   end
 
   def run_params
